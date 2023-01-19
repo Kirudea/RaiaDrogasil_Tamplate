6b224ec576e0a26aa62a94cca4af35a572507c48
Dev <dev@pactomais.com.br>
2023-01-19 18:16:15 -0300

    Ambiente preparado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+//
+
+class App extends React.Component {
+    render() {
+        return (
+            <form action="/" method="POST">
+                <input name="nome" type="text"/><br/>
+                <input name="email" type="email"/><br/>
+                <input name="senha" type="password"/><br/>
+                <input name="conf_senha" type="password"/><br/>
+                <input name="cpf" type="number"/><br/><br/>
+
+                <input type="submit" value="Cadastrar"/>
+            </form>
+        );
+    }
+}
+
+//
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>KILDERE</h1>);
+root.render(<App/>);
