56bc7efd2273c6f14c6a940f15cb9ad70cef5d4f
Kirudea <kilderehenriquedp@gmail.com>
2023-01-20 18:35:32 -0300

    upgrade form validations

--- src/index.js
+++ src/index.js
@@ -1,25 +1,73 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 //
 
+function InputForm(props) {  
+  return (
+    <input type={props.type} class="form-control" pattern={props.pattern} minLength={props.minLength} maxLength={props.maxLength} required
+      placeholder={props.placeholder} onChange={props.onChange}/>
+  );
+}
+
+//
+
 class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: null,
+      email: null,
+      cpf: null,
+      senha: null,
+      conf_senha: null,
+    }
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event, campo) {
+    this.setState({[campo]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(
+        this.state.nome + "\n" + 
+        this.state.email + "\n" + 
+        this.state.cpf + "\n" + 
+        this.state.senha + "\n" + 
+        this.state.conf_senha 
+      );
+      
+    
+  }
+
+  validConfSenha(){
+
+  }
+
   render() {
     return (
       <div className="app">
         <div class="register-box" id="form">
           <div class="card card-outline card-primary">
             <div class="card-header text-center">
-              <a href="" class="h1">
-                <b>LOGO</b>
+              <a href="###" class="h1">
+                <img id="logo" src="icon.png" alt="LOGO" width="30%"/>
               </a>
+              <div>
+                <h3>Auditoria FP</h3>
+              </div>
             </div>
             <div class="card-body">
-              <p class="login-box-msg">Cadastrar novo usuário</p>
-              <form action="" method="">
+              <p class="login-box-msg"><b>Cadastrar novo usuário</b></p>
+              <form action="" method="" onSubmit={this.handleSubmit}>
                 <div class="input-group mb-3">
-                  <input type="text" class="form-control" required placeholder="Nome completo"/>
+                  <InputForm type="text" placeholder="Nome completo" onChange={(e) => this.handleChange(e, "name")}/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -29,7 +77,7 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="input-group mb-3">
-                  <input type="email" class="form-control" required placeholder="exemplo@mail.com"/>
+                  <InputForm type="email" placeholder="exemplo@mail.com" onChange={(e) => this.handleChange(e, "email")}/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -39,7 +87,7 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="input-group mb-3">
-                  <input type="text" class="form-control" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" minLength="14" maxLength="14" pro required placeholder="xxx.xxx.xxx-xx"/>
+                  <InputForm type="text" placeholder="xxx.xxx.xxx-xx" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" minLength="14" maxLength="14" onChange={(e) => this.handleChange(e, "name")}/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -49,7 +97,8 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="input-group mb-3">
-                  <input type="password" class="form-control" minLength="8" required placeholder="Senha"/>
+                  <InputForm type="password" placeholder="Senha" onChange={(e) => this.handleChange(e, "password")}/>
+                  <input type="password" class="form-control" name="senha" minLength="8" required placeholder="Senha"/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -59,7 +108,7 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="input-group mb-3">
-                  <input type="password" class="form-control" minLength="8" required placeholder="Confirme senha"/>
+                  <input type="password" class="form-control" name="confSenha" minLength="8" required placeholder="Confirme senha" onChange={this.validConfSenha}/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -69,15 +118,19 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="row">
-                  <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">
-                      Register
-                    </button>
+                  <div class="col-6">
+                    <div class="col-10">
+                      <button type="submit" class="btn btn-primary btn-block">
+                        Register
+                      </button>
+                    </div>
                   </div>
-                  <div class="col-8">
-                    <button type="submit" class="btn btn-default float-right">
-                      Cancelar
-                    </button>
+                  <div class="col-6">
+                    <div class="col-10 float-right">
+                      <a href="###" class="btn btn-default btn-block">
+                        Cancelar
+                      </a>
+                    </div>
                   </div>
                 </div>
               </form>
