68e87fa7e1206ef8229973fea0a37522f33e5b47
Kirudea <kilderehenriquedp@gmail.com>
2023-01-21 17:27:44 -0300

    Melhorado validações e Logo não carrega

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+//NOVO
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-//
+function InputForm(props) {  
+  return (
+    <input type={props.type} name={props.name} id={props.id} class="form-control" pattern={props.pattern} minLength={props.minLength} maxLength={props.maxLength} required
+      placeholder={props.placeholder} onChange={props.onChange}/>
+  );
+}
 
 function FormatCPF(cpf) {
     var accept_chars = /[^0-9]/gi; //Somente nums
@@ -21,16 +27,28 @@ function FormatCPF(cpf) {
     return cpf;
 }
 
-//
+function ValidarEmail(email){
+  var resp = "";
+  email = email.split("@");
 
-function InputForm(props) {  
-  return (
-    <input type={props.type} id={props.id} class="form-control" pattern={props.pattern} minLength={props.minLength} maxLength={props.maxLength} required
-      placeholder={props.placeholder} onChange={props.onChange}/>
-  );
+  if(email.length === 2){
+    if(email[0].length > 0 & email[1].length > 0){
+      email = email[1].split(".");
+
+      if(email.length > 1){
+        if(email[0].length > 0 & email[1].length > 1){
+         return null;
+        }else
+          resp = "O email deve conter ao menos 1 caractere antes e 2 depois do \".\".";
+      }else
+        resp = "O email deve conter ao menos um \".\" após o \"@\".";
+    }else
+      resp = "O email deve conter algo antes e depois do \"@\".";
+  }else
+    resp = "O email deve conter um \"@\".";
+  
+  return resp;
 }
-
-//
 
 class UserForm extends React.Component {
   constructor(props) {
@@ -48,42 +66,54 @@ class UserForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  validarSenhas(novoValor, campo) {
-    var validConfSenha= "valid";
-    var disabled = false;
-    
-    document.getElementById(campo).className = "form-control is-"+(novoValor.length >= 8?"":"in")+"valid";
- 
-    if(novoValor !== this.state.senha & novoValor !== this.state.conf_senha)
-      validConfSenha = "invalid";
+  setInvalidCSS(componenteId, invalido){
+    document.getElementById(componenteId).className = "form-control is-"+(invalido?"in":"")+"valid";
+  }
+
+  handleChange(event) {
+    var invalido = false;
+    switch(event.target.name) {
+      case "nome":
+        event.target.value = event.target.value.trimStart();
+        invalido = event.target.value === "" | event.target.value == null;
+        break;
       
-    document.getElementById("conf_senha").className = "form-control is-"+validConfSenha;
-    document.getElementById("submit-btt").disabled = disabled;
+      case "cpf":
+        event.target.value = FormatCPF(event.target.value);
+        invalido = event.target.value.length !== 14
+        break;
+      
+      case "email":
+        invalido = ValidarEmail(event.target.value) !== null;  
+        break
+
+      case "senha":
+      case "conf_senha":
+        event.target.value = event.target.value.trim();
+        invalido = event.target.value.length < 8;
+        this.setInvalidCSS("conf_senha", event.target.value !== this.state.senha & event.target.value !== this.state.conf_senha);
+        break
+        
+      default:
+    }
+
+    this.setInvalidCSS(event.target.id, invalido);
+    this.setState({[event.target.name]: event.target.value});
   }
 
-  handleChange(event, campo) {
-    if(campo === "cpf")
-    event.target.value = FormatCPF(event.target.value);
-    
-    this.setState({[campo]: event.target.value});
-  
-    if(campo === "senha" | campo === "conf_senha")
-      this.validarSenhas(event.target.value, campo);
-  }
-
-  handleSubmit(event) {
+  handleSubmit() {
+    //state é o JSON!
+    //Email existente?
+    //CPF existente? OU deixa pro back?
     alert(
-        this.state.nome + "\n" + 
-        this.state.email + "\n" + 
-        this.state.cpf + "\n" + 
-        this.state.senha + "\n" + 
-        this.state.conf_senha 
-      );
+      this.state.nome.trim() + "\n" + 
+      this.state.email.trim() + "\n" + 
+      this.state.cpf.trim() + "\n" + 
+      this.state.senha.trim() + "\n" + 
+      this.state.conf_senha.trim()
+    );
 
-      //state é o JSON!
-      //Email existente?
-      //CPF existente? OU deixa pro back?
-
+    //alert(JSON.stringify(this.state));
   }
 
   render() {
@@ -93,7 +123,7 @@ class UserForm extends React.Component {
           <div class="card card-outline card-primary">
             <div class="card-header text-center">
               <a href="/" class="h1">
-                <img id="logo" src="img/icon.png" alt="LOGO" width="30%"/>
+                <img id="logo" src="icon.png" alt="LOGO" width="30%"/>
               </a>
               <div>
                 <h3>Auditoria FP</h3>
@@ -101,9 +131,9 @@ class UserForm extends React.Component {
             </div>
             <div class="card-body">
               <p class="login-box-msg"><b>Cadastrar novo usuário</b></p>
-              <form onSubmit={this.handleSubmit}>
+              <form onSubmit={this.handleSubmit} autoComplete="on" /*method="POST"*/>
                 <div class="input-group mb-3">
-                  <InputForm type="text" placeholder="Nome completo" onChange={(e) => this.handleChange(e, "nome")}/>
+                  <InputForm type="text" name="nome" id="nome" placeholder="Nome completo" onChange={(e) => this.handleChange(e)}/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -113,7 +143,7 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="input-group mb-3">
-                  <InputForm type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="exemplo@mail.com" onChange={(e) => this.handleChange(e, "email")}/>
+                  <InputForm type="email" name="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="meu@email.com" onChange={(e) => this.handleChange(e)}/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -123,7 +153,7 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="input-group mb-3">
-                  <InputForm type="text" placeholder="Informe CPF" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" minLength="14" maxLength="14" onChange={(e) => this.handleChange(e, "cpf")}/>
+                  <InputForm type="text" name="cpf" id="cpf" placeholder="Informe CPF" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" minLength="14" maxLength="14" onChange={(e) => this.handleChange(e)}/>
                   <div class="input-group-append">
                     <div class="input-group-text">
                         <span>
@@ -133,10 +163,24 @@ class UserForm extends React.Component {
                   </div>
                 </div>
                 <div class="input-group mb-3">
-                  <InputForm type="password" id="senha" placeholder="Senha" minLength="8" onChange={(e) => this.handleChange(e, "senha")}/>
+                  <InputForm type="password" name="senha" id="senha" placeholder="Senha" minLength="8" onChange={(e) => this.handleChange(e)}/>
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span>
+                            <i class="gg-lock"></i>
+                        </span>
+                    </div>
+                  </div>
                 </div>
                 <div class="input-group mb-3">
-                <InputForm type="password" id="conf_senha" placeholder="Confirme senha" minLength="8" onChange={(e) => this.handleChange(e, "conf_senha")}/>
+                  <InputForm type="password" name="conf_senha" id="conf_senha" placeholder="Confirme senha" minLength="8" onChange={(e) => this.handleChange(e)}/>
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span>
+                            <i class="gg-lock"></i>
+                        </span>
+                    </div>
+                  </div>
                 </div>
                 <div class="row">
                   <div class="col-6">
@@ -162,8 +206,6 @@ class UserForm extends React.Component {
     );
   }
 }
-
-//
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<UserForm />);
