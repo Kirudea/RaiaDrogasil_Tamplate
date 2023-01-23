99c4da3bc6d143e3b86699645ffefdea8defbdfe
Kirudea <kilderehenriquedp@gmail.com>
2023-01-23 00:50:45 -0300

    Ajustes validação

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 function InputForm(props) {  
   return (
     <input type={props.type} name={props.name} id={props.id} class="form-control" pattern={props.pattern} minLength={props.minLength} maxLength={props.maxLength} required
-      placeholder={props.placeholder} onChange={props.onChange}/>
+      placeholder={props.placeholder} onChange={props.onChange} onClick={props.onChange}/>
   );
 }
 
@@ -69,7 +69,7 @@ class UserForm extends React.Component {
       case "conf_senha":
         event.target.value = event.target.value.trim();
         this.setInvalidCSS(event.target.id, event.target.value.length < 8);
-        this.setInvalidCSS("conf_senha", event.target.value !== this.state.senha & event.target.value !== this.state.conf_senha);
+        this.setInvalidCSS("conf_senha", event.target.value.length < 8 | (event.target.value !== this.state.senha & event.target.value !== this.state.conf_senha));
         break
         
       default:
@@ -87,87 +87,106 @@ class UserForm extends React.Component {
 
   render() {
     return (
-      <div class="content-wrapper">
-        <div class="register-box" id="form">
-          <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-              <a href="/" class="h1">
-                <img id="logo" src="icon.png" alt="LOGO" width="30%"/>
+      <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="#sidebar" id="sidebar-btt" class="nav-link" role="button">
+                <b>SIDE BAR</b>
               </a>
-              <div>
-                <h3>Auditoria FP</h3>
+            </li>
+            <li class="nav-item">
+              <a href="#nome" class="nav-link" role="button">
+                <b>FOCAR</b>
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <aside id="sidebar" class="main-sidebar sidebar-dark-primary elevation-4"></aside>
+        
+        <div class="content-wrapper">
+          <div class="register-box" id="form">
+            <div class="card card-outline card-primary">
+              <div class="card-header text-center">
+                <a href="/" class="h1">
+                  <img id="logo" src="img/icon.png" alt="LOGO" width="30%"/>
+                </a>
+                <div>
+                  <h3>Auditoria FP</h3>
+                </div>
               </div>
-            </div>
-            <div class="card-body">
-              <p class="login-box-msg"><b>Cadastrar novo usuário</b></p>
-              <form onSubmit={(e) => this.handleSubmit(e)} onReset={(e) => this.cleanInvalidCSS(e)} autoComplete="on" /*method="POST"*/>
-                <div class="input-group mb-3">
-                  <InputForm type="text" name="nome" id="nome" pattern="[a-zA-Z\s]+" placeholder="Nome completo" onChange={(e) => this.handleChange(e)}/>
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span>
-                            <i class="gg-user"></i>
-                        </span>
+              <div class="card-body">
+                <p class="login-box-msg"><b>Cadastrar novo usuário</b></p>
+                <form action="#senha" onSubmit={(e) => this.handleSubmit(e)} onReset={(e) => this.cleanInvalidCSS(e)} autoComplete="on" /*method="POST"*/>
+                  <div class="input-group mb-3">
+                    <InputForm type="text" name="nome" id="nome" pattern="[a-zA-Z\s]+" placeholder="Nome completo" onChange={(e) => this.handleChange(e)}/>
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                          <span>
+                              <i class="gg-user"></i>
+                          </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="input-group mb-3">
-                  <InputForm type="email" name="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="meu@email.com" onChange={(e) => this.handleChange(e)}/>
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span>
-                            <i class="gg-mail"></i>
-                        </span>
+                  <div class="input-group mb-3">
+                    <InputForm type="text" name="cpf" id="cpf" placeholder="Informe CPF" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" minLength="14" maxLength="14" onChange={(e) => this.handleChange(e)}/>
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                          <span>
+                              <i class="gg-info"></i>
+                          </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="input-group mb-3">
-                  <InputForm type="text" name="cpf" id="cpf" placeholder="Informe CPF" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" minLength="14" maxLength="14" onChange={(e) => this.handleChange(e)}/>
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span>
-                            <i class="gg-info"></i>
-                        </span>
+                  <div class="input-group mb-3">
+                    <InputForm type="email" name="email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="exemplo@email.com" onChange={(e) => this.handleChange(e)}/>
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                          <span>
+                              <i class="gg-mail"></i>
+                          </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="input-group mb-3">
-                  <InputForm type="password" name="senha" id="senha" minLength="8" pattern=".{8,}" placeholder="Senha" onChange={(e) => this.handleChange(e)}/>
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span>
-                            <i class="gg-lock"></i>
-                        </span>
+                  <div class="input-group mb-3">
+                    <InputForm type="password" name="senha" id="senha" minLength="8" pattern=".{8,}" placeholder="Senha" onChange={(e) => this.handleChange(e)}/>
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                          <span>
+                              <i class="gg-lock"></i>
+                          </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="input-group mb-3">
-                  <InputForm type="password" name="conf_senha" id="conf_senha" minLength="8" pattern=".{8,}" placeholder="Confirme senha" onChange={(e) => this.handleChange(e)}/>
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span>
-                            <i class="gg-lock"></i>
-                        </span>
+                  <div class="input-group mb-3">
+                    <InputForm type="password" name="conf_senha" id="conf_senha" minLength="8" pattern=".{8,}" placeholder="Confirme senha" onChange={(e) => this.handleChange(e)}/>
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                          <span>
+                              <i class="gg-lock"></i>
+                          </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="row">
-                  <div class="col-6">
-                    <div class="col-10">
-                      <button type="submit" id="submit-btt" class="btn btn-primary btn-block">
-                        Cadastrar
-                      </button>
+                  <div class="row">
+                    <div class="col-6">
+                      <div class="col-10">
+                        <button type="submit" id="submit-btt" class="btn btn-primary btn-block">
+                          Cadastrar
+                        </button>
+                      </div>
+                    </div>
+                    <div class="col-6">
+                      <div class="col-10 float-right">
+                        <button type="reset" class="btn btn-default btn-block">
+                          Limpar
+                        </button>
+                      </div>
                     </div>
                   </div>
-                  <div class="col-6">
-                    <div class="col-10 float-right">
-                      <button type="reset" class="btn btn-default btn-block">
-                        Limpar
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
